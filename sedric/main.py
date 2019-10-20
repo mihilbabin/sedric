803cb7a7471126f1a9ab7956198bea7aab4ae858
Michael Babin <iamsuperman@ukr.net>
2019-10-20 20:52:03 +0300

    added class and methods for image

--- sedric/main.py
+++ sedric/main.py
@@ -3,9 +3,13 @@ from image import Image
 
 
 def main():
-    img_path = Path(__file__).parent / 'lanes.jpg'
-    img = Image(str(img_path.absolute()))
-    img.show()
+    img_path = (Path(__file__).parent / 'lanes.jpg').absolute()
+    img = Image.from_file(str(img_path))
+    grayscale = img.grayscale()
+    blurred = grayscale.blur()
+    blurred.show('blurred')
+    grayscale.show('grayscale')
+    img.show('Original')
 
 
 if __name__ == "__main__":
